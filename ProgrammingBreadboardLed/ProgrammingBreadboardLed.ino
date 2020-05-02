2fee3f6020f00f4bf048b249d2e4ad2059aaf6d9
Nieuwburg,Patrick P.E.J. van <p.vannieuwburg@student.fontys.nl>
2020-05-02 15:51:45 +0200

    Add the program that makes a led blink on breadboard

---
+++ ProgrammingBreadboardLed/ProgrammingBreadboardLed.ino
@@ -0,0 +1,11 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  //Zet led aan -> 5 sec aan -> let uit -> 1 sec uit
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(2000);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                       
+}
